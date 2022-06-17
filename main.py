2aae09bb1d8350bd0062036c69f7a7118b5c54fa
alejandro <54691090+Ale-cm@users.noreply.github.com>
2022-06-17 00:51:56 -0300

    Feat: ingreso de 5 numeros enteros por el usuario + comienzo del proyecto

---
+++ main.py
@@ -0,0 +1,10 @@
+# Autores: Alejandro cabrera
+#Fecha: 16/06/22
+#version:0.0.1
+#Description: integrador 
+import funciones as fun
+if __name__ == "__main__":
+    print("Buen dia: ")
+    print("Por favor ingrese 5 numeros: ")
+    lista = fun.list()
+    print(" cuidado ", lista)
